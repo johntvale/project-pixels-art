bc9b4e8d2def4b6dc6bc089437362ce62ac00435
johntvale <johntvale@gmail.com>
2021-06-01 22:34:24 -0400

    requisito 3 resolvido

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function pagCarregada() {
   alert('Carregada.');
 }
 window.onload = pagCarregada;
+
+function criarDivs() {
+  let tablePrincipal = document.querySelector('#quadroDePixels');
+  for (let index1 = 0; index1 < 5; index1 +=1) {
+    for (let index2 = 0; index2 < 5; index2 +=1) {
+      let linha = document.createElement('div');
+      linha.className = 'pixels';
+      tablePrincipal.appendChild(linha);
+    }
+  }
+}
+
+criarDivs();
